a4a8fdb19b0c1bc001f08a2e7cdc285957284031
Dmitry <dru4inin.dmitry@yandex.ru>
2023-12-06 15:17:34 +0300

    fix linters

--- service/api/my_models.py
+++ service/api/my_models.py
@@ -20,12 +20,13 @@ def user_knn_model(user_id: int):
         user_recommendations = userknn_recos[userknn_recos["user_id"] == user_id]["item_id"].to_list()
         if user_recommendations:
             if len(user_recommendations) >= 10:
-                return user_recommendations[:10]  # Вернуть первые 10 рекомендаций
+                user_recommendations = user_recommendations[:10]  # Вернуть первые 10 рекомендаций
             else:
                 num_popular_recos = 10 - len(user_recommendations)
                 popular_recos_subset = popular_recos[:num_popular_recos]
-                return user_recommendations + popular_recos_subset + [0] * num_popular_recos
+                user_recommendations = user_recommendations + popular_recos_subset + [0] * num_popular_recos
         else:
-            return popular_recos[:10]  # Вернуть первые 10 популярных рекомендаций
+            user_recommendations = popular_recos[:10]  # Вернуть первые 10 популярных рекомендаций
     else:
-        return popular_recos[:10]
+        user_recommendations = popular_recos[:10]
+    return user_recommendations
