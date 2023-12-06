a3992c073d001ef921a6d8415ea47da089412ed9
Dmitry <dru4inin.dmitry@yandex.ru>
2023-12-06 15:15:48 +0300

    fix bugs

--- service/api/my_models.py
+++ service/api/my_models.py
@@ -1,21 +1,23 @@
 import os
+
 import pandas as pd
 
 userknn_recos = pd.DataFrame()
-unique_users_userknn = list()
+unique_users_userknn = []
 PATH = "recos/userknn_recos.csv"
 if os.path.exists(PATH):
     userknn_recos = pd.read_csv(PATH)
-    unique_users_userknn = userknn_recos['user_id'].unique()
+    unique_users_userknn = userknn_recos["user_id"].unique()
 
 popular_recos = pd.DataFrame()
 PATH2 = "recos/popular.csv"
 if os.path.exists(PATH2):
-    popular_recos = pd.read_csv(PATH2)['item_id'].to_list()
+    popular_recos = pd.read_csv(PATH2)["item_id"].to_list()
 
-def user_knn_model(user_id):
+
+def user_knn_model(user_id: int):
     if user_id in unique_users_userknn:
-        user_recommendations = userknn_recos[userknn_recos['user_id'] == user_id]['item_id'].to_list()
+        user_recommendations = userknn_recos[userknn_recos["user_id"] == user_id]["item_id"].to_list()
         if user_recommendations:
             if len(user_recommendations) >= 10:
                 return user_recommendations[:10]  # Вернуть первые 10 рекомендаций
@@ -27,4 +29,3 @@ def user_knn_model(user_id):
             return popular_recos[:10]  # Вернуть первые 10 популярных рекомендаций
     else:
         return popular_recos[:10]
-    
--- service/api/views.py
+++ service/api/views.py
@@ -6,8 +6,9 @@ from fastapi.security import OAuth2PasswordBearer
 from pydantic import BaseModel
 
 from service.api.exceptions import AuthorizationError, ModelNotFoundError, UserNotFoundError
-from service.log import app_logger
 from service.api.my_models import user_knn_model
+from service.log import app_logger
+
 
 class RecoResponse(BaseModel):
     user_id: int
@@ -73,7 +74,7 @@ async def get_reco(
         reco = list(range(k_recs))
     elif model_name == "random":
         reco = list(random.sample(range(1001), k_recs))
-    elif model_name == "user_knn": 
+    elif model_name == "user_knn":
         reco = user_knn_model(user_id)
     else:
         raise ModelNotFoundError(error_message=f"Model {model_name} not found")
